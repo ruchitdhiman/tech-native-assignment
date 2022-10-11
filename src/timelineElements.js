f21827a561d64b51e758da687a45229d1ea6220e
ruchitdhiman <87163293+ruchitdhiman@users.noreply.github.com>
2022-10-11 11:18:14 +0530

    first commit

---
+++ src/timelineElements.js
@@ -0,0 +1,47 @@
+let timelineElements = [
+    {
+      id: 1,
+      title: "ENQUIRY",
+      location: "Dehradun,India",
+      description:
+        "1.conceptualize 2.sketch 3.color-profiles",
+      
+      date: "28JAN -1 FEB",
+      icon: "work",
+      
+    },
+    {
+      id: 2,
+      title: "BOOKING",
+      location: "dehradun,india",
+      description:
+        "1.HTML 2. write the js",
+     
+      date: "2 feb - 9 feb",
+      icon: "work",
+    },
+    {
+      id: 3,
+      title: "RETAIL",
+      location: "dehradun,india",
+      description:
+        "1.adverstise 2. spam links",
+    
+      date: "10feb-14 feb",
+      icon: "work",
+    },
+    {
+      id: 4,
+      title: "DELIVERY",
+      location: "dehradun,India",
+      description:
+        "1.eat 2.delivery-task",
+      
+      date: "9feb-16feb",
+      icon: "school",
+      
+    },
+   
+  ];
+  
+  export default timelineElements;
